d9bf130005af428d9dccbc20b52c9581945f7952
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-06-30 05:00:29 -0400

    Add initial files

---
+++ app.js
@@ -0,0 +1,27 @@
+fetch("data.json")
+.then(function(response){
+    return response.json()
+})
+.then(function(scores){
+    console.log(scores)
+
+    let placeholder = document.querySelector("#data-output");
+    let scorePlaceHolcer = document.querySelector('#scoreSum')
+    let out = "";
+    let scoreSum = 0;
+
+    for(let score of scores){
+        out += `
+        <li><img src='${score.icon}'> <span id='category${scoreSum}' class='text'>${score.category} </span><span><b> ${score.score}</b><span> / 100</span></span></li>
+        `;
+
+        scoreSum += Math.floor(score.score/scores.length);      
+       
+
+        placeholder.innerHTML = out;
+        scorePlaceHolcer.innerHTML = scoreSum
+
+    }
+
+
+})
